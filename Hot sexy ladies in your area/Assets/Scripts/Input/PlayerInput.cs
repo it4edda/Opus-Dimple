0888335df2700f524da1582e206f76fa9fc45592
William Edmalm <nilswilliam2005@gmail.com>
2023-04-29 00:49:45 +0200

    whammy 1

--- Hot sexy ladies in your area/Assets/Scripts/Input/PlayerInput.cs
+++ Hot sexy ladies in your area/Assets/Scripts/Input/PlayerInput.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public static class PlayerInput
+public class PlayerInput : MonoBehaviour
 {
+    [Range(1, 2)]
+    [SerializeField] private int controllerNumber;
 
+    public int GetControllerNumber() => controllerNumber;
+
+    
+    public void SetControllerNumber(int newControllerNumber)
+    {
+        if (newControllerNumber == 1 || newControllerNumber == 2)
+        {
+            controllerNumber = newControllerNumber;
+        }
+    }
+    
+    public Vector2 GetLeftStickVector()
+    {
+        float xAxis = 1; //Input.GetAxisRaw("LeftStickHorizontal" + controllerNumber);         //FIX THIS PLEAASEE
+        float yAxis = 1; // Input.GetAxisRaw("LeftStickVertical"   + controllerNumber);
+
+        Vector2 leftStickInput = new Vector2(xAxis, yAxis);
+
+        if (leftStickInput.magnitude < 0.2f) { return Vector2.zero; }
+
+        return leftStickInput;
+    }
 }
